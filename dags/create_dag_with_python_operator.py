ec05eceadeed973bd05ad98760470aad5ccc86d5
Luis <manuelluisdelosreyes@gmail.com>
2025-11-29 08:47:00 +0800

    our_first_dag_with_python_operator_v04

--- dags/create_dag_with_python_operator.py
+++ dags/create_dag_with_python_operator.py
@@ -8,7 +8,8 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-def greet(name, age):
+def greet(age, ti):
+    name = ti.xcom_pull(task_ids='get_name')
     print(f"Hello World! My name is {name} and I am {age} years old.")
     
 def get_name():
@@ -16,20 +17,20 @@ def get_name():
 
 with DAG(
     default_args = default_args,
-    dag_id = 'our_first_dag_with_python_operator_v03',
+    dag_id = 'our_first_dag_with_python_operator_v04',
     description = 'Our first DAG using python operator',
     start_date = datetime(2025, 11, 28),
     schedule_interval = '@daily' 
 ) as dag:
-    # task1 = PythonOperator(
-    #     task_id = 'greet',
-    #     python_callable = greet,
-    #     op_kwargs = {'name': 'Luis', 'age': 24}
-    # )
+    task1 = PythonOperator(
+        task_id = 'greet',
+        python_callable = greet,
+        op_kwargs = {'name': 'Luis', 'age': 24}
+    )
     
     task2 = PythonOperator(
         task_id = 'get_name',
         python_callable = get_name
     )
     
-    task2
+    task2 >> task1
