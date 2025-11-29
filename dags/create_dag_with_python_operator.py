a3daa613e249d777e3a3bd46a711a68bff4c64a8
Luis <manuelluisdelosreyes@gmail.com>
2025-11-29 08:33:03 +0800

    our_first_dag_with_python_operator_v03

--- dags/create_dag_with_python_operator.py
+++ dags/create_dag_with_python_operator.py
@@ -10,18 +10,26 @@ default_args = {
 
 def greet(name, age):
     print(f"Hello World! My name is {name} and I am {age} years old.")
+    
+def get_name():
+    return 'Luis'
 
 with DAG(
     default_args = default_args,
-    dag_id = 'our_first_dag_with_python_operator_v02',
+    dag_id = 'our_first_dag_with_python_operator_v03',
     description = 'Our first DAG using python operator',
     start_date = datetime(2025, 11, 28),
     schedule_interval = '@daily' 
 ) as dag:
-    task1 = PythonOperator(
-        task_id = 'greet',
-        python_callable = greet,
-        op_kwargs = {'name': 'Luis', 'age': 24}
+    # task1 = PythonOperator(
+    #     task_id = 'greet',
+    #     python_callable = greet,
+    #     op_kwargs = {'name': 'Luis', 'age': 24}
+    # )
+    
+    task2 = PythonOperator(
+        task_id = 'get_name',
+        python_callable = get_name
     )
     
-    task1
+    task2
