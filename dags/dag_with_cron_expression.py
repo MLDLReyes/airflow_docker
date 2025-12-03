0c17602139255b7fba626224d92020e1d22abc2a
Luis <manuelluisdelosreyes@gmail.com>
2025-12-03 08:29:17 +0800

    dag_with_cron_expression_v01

---
+++ dags/dag_with_cron_expression.py
@@ -0,0 +1,22 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+default_args = {
+    'owner': 'luis',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5)
+}
+
+with DAG(
+    default_args=default_args,
+    dag_id='dag_with_cron_expression_v01',
+    start_date=datetime(2025, 11, 30),
+    schedule_interval='@daily'
+) as dag:
+    task1 = BashOperator(
+        task_id='task1',
+        bash_command="echo dag with cron expression."
+    )
+    task1
