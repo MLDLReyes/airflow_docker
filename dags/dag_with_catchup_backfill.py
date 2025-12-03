b37636e978b845c128e5f135af54b3067537de14
Luis <manuelluisdelosreyes@gmail.com>
2025-12-03 08:14:43 +0800

    dag_with_catchup_backfill_v02

--- dags/dag_with_catchup_backfill.py
+++ dags/dag_with_catchup_backfill.py
@@ -10,13 +10,16 @@ default_args = {
 }
 
 with DAG(
-    dag_id="dag_with_catchup_backfill_v01",
+    dag_id="dag_with_catchup_backfill_v02",
     default_args=default_args,
     start_date=datetime(2025, 11, 25),
     schedule_interval='@daily',
-    catchup=True
+    catchup=False
 ) as dag:
     task1 = BashOperator(
         task_id='task1',
         bash_command="echo This is a simple bash command."
     )
+    
+#run in terminal this command to backfill
+#docker exec -it airflow_docker-airflow-webserver-1 airflow dags backfill dag_with_catchup_backfill_v02 --start-date 2025-11-26 --end-date 2025-12-02
