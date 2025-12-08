06b75f73d1ff1f38bb12e6e9a80f5b3c63256c0e
Luis <manuelluisdelosreyes@gmail.com>
2025-12-08 09:01:02 +0800

    dag_with_postgres_operator_v03

--- dags/dag_with_postgres_operator.py
+++ dags/dag_with_postgres_operator.py
@@ -10,7 +10,7 @@ default_args = {
 }
 
 with DAG(
-    dag_id='dag_with_postgres_operator_v02',
+    dag_id='dag_with_postgres_operator_v03',
     default_args=default_args,
     start_date=datetime(2025, 12, 6),
     schedule_interval='0 0 * * *',
@@ -33,4 +33,11 @@ with DAG(
             insert into dag_runs (dt, dag_id) values ('{{ ds }}', '{{ dag.dag_id }}')
         """
     )
-    task1 >> task2
+    task3 = PostgresOperator(
+        task_id='delete_data_from_table',
+        postgres_conn_id='postgres_localhost',
+        sql="""
+            delete from dag_runs where dt = '{{ ds }}' and dag_id = '{{ dag.dag_id }}'
+        """
+    )
+    task1 >> task3 >>task2
